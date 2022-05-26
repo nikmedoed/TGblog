5b21ee1c40419c5e0c062eb9f4759fa78a3f675a
Nikita <grafniksan@gmail.com>
2022-05-26 18:18:05 +0300

    vk online unique upgrade

--- vk_online_monitoring/vk_online_monitoring.py
+++ vk_online_monitoring/vk_online_monitoring.py
@@ -1,38 +1,98 @@
-import vk_api
-from time import sleep
+import csv
 from datetime import datetime
+from time import sleep
+
+import vk_api
+
+my_file = "unique_online_log.csv"
+
+group_id = "cmctvc"
+group_file = f"unique_online_group_log.csv"
 
 
-def auth_handler():
-    key = input("Enter authentication code: ")
-    remember_device = True
-    return key, remember_device
+class VKconnector():
+    def __init__(self, login, password):
+        def auth_handler():
+            key = input("Enter authentication code: ")
+            remember_device = True
+            return key, remember_device
 
+        self.vk_session = vk_api.VkApi(login, password, auth_handler=auth_handler)
+        while 1:
+            try:
+                self.vk_session.auth(token_only=True)
+                break
+            except vk_api.AuthError as error_msg:
+                print(error_msg)
 
-login, password = open('../../vk_login_pass.txt', 'r').read().split() # replace with your 'login', 'pass'
-vk_session = vk_api.VkApi(login, password, auth_handler=auth_handler)
+        self.friends_fieldnames = ('datetime', *self.get_friends_ids())
+        self.group_fieldnames = 'datetime', *[i['id'] for i in self.get_group_users()['items']]
 
-try:
-    vk_session.auth(token_only=True)
-except vk_api.AuthError as error_msg:
-    print(error_msg)
+    def get_friends_ids(self):
+        api = self.vk_session.get_api()
+        return api.friends.get()['items']
 
-while 1:
-    api = vk_session.get_api()
-    friends = len(api.friends.getOnline())
-    while 1:
-        try:
-            tools = vk_api.VkTools(vk_session)
-            cmctvc = tools.get_all_slow('groups.getMembers', 500, {'group_id': "cmctvc", 'fields': 'online'})
-            break
-        except:
-            pass
+    def get_friends_ids_online(self):
+        api = self.vk_session.get_api()
+        return api.friends.getOnline()
+
+    def get_group_users(self):
+        while 1:
+            try:
+                tools = vk_api.VkTools(self.vk_session)
+                group = tools.get_all_slow(
+                    'groups.getMembers',
+                    1000,   # можно попробовать и больше, этот метод должен позволять,
+                            # но у меня были проблемы, да и мне не надо
+                    {'group_id': group_id,
+                     'fields': 'online'}
+                )
+                break
+            except:
+                pass
         sleep(1)
-    try:
-        cmctvc = len([i for i in cmctvc['items'] if i['online']])
-        print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\t{friends}\t{cmctvc}')
-        with open('log.csv', "a+", encoding="utf8") as f:
-            f.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\t{friends}\t{cmctvc}\n')
-    except Exception as e:
-        print(e)
-    sleep(60)
+        return group
+
+    def get_friends_online(self):
+        return {i: True for i in self.get_friends_ids_online()}
+
+    def get_group_online(self):
+        return {i['id']: True for i in self.get_group_users()['items'] if i['online']}
+
+
+def write_headers(file, names):
+    with open(file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=names)
+        writer.writeheader()
+
+
+def write_row(file, fields, data):
+    with open(file, 'a+', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, extrasaction='ignore', fieldnames=fields)
+        writer.writerow(data)
+
+
+if __name__ == "__main__":
+    login, password = open('../../vk_login_pass.txt', 'r').read().split()
+    vk_conn = VKconnector(login, password)
+
+    write_headers(my_file, vk_conn.friends_fieldnames)
+    write_headers(group_file, vk_conn.group_fieldnames)
+
+    activities = (
+        (my_file, vk_conn.friends_fieldnames, vk_conn.get_friends_online),
+        (group_file, vk_conn.group_fieldnames, vk_conn.get_group_online)
+    )
+
+    while 1:
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(now, end="\t")
+        for i in activities:
+            try:
+                online=i[2]()
+                write_row(i[0], i[1], {'datetime': now, **online})
+                print(len(online), end="\t")
+            except Exception as e:
+                print(e)
+        print()
+        sleep(60)
